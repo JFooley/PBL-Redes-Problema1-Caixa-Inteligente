8592237650ebe2de6ba1595b6fdb691b2c88e990
João Gabriel <gabriel.lima.almeida@gmail.com>
2023-09-03 21:19:41 -0300

    Fix: IDs dos dados

--- Dados.py
+++ Dados.py
@@ -3,7 +3,16 @@ dados = {
     '2': {'nome' : 'Carne'  , 'codigo' : '2', 'preco' : 20.0, 'stock' : 10},
     '3': {'nome' : 'Leite'  , 'codigo' : '3', 'preco' : 6.00, 'stock' : 10},
     '4': {'nome' : 'Ovo'    , 'codigo' : '4', 'preco' : 0.50, 'stock' : 10},
-    '5': {'nome' : 'Feijão' , 'codigo' : '5', 'preco' : 9.50, 'stock' : 10}
+    '5': {'nome' : 'Feijão' , 'codigo' : '5', 'preco' : 9.50, 'stock' : 10},
+    'E20000172211010218905459': {'nome' : 'Arroz'  , 'codigo' : 'E20000172211010218905459', 'preco' : 5.50, 'stock' : 10},
+    'E20000172211010118905454': {'nome' : 'Carne'  , 'codigo' : 'E20000172211010118905454', 'preco' : 20.0, 'stock' : 10},
+    'E20000172211011718905474': {'nome' : 'Leite'  , 'codigo' : 'E20000172211011718905474', 'preco' : 6.00, 'stock' : 10},
+    'E2000017221101321890548C': {'nome' : 'Ovo'    , 'codigo' : 'E2000017221101321890548C', 'preco' : 0.50, 'stock' : 10},
+    'E20000172211009418905449': {'nome' : 'Feijão' , 'codigo' : 'E20000172211009418905449', 'preco' : 9.50, 'stock' : 10},
+    'E20000172211012518905484': {'nome' : "Laranja", 'codigo' : 'E20000172211012518905484', 'preco' : 3.00, 'stock': 12},
+    'E20000172211011118905471': {'nome' : "Uva"    , 'codigo' : 'E20000172211011118905471', 'preco' : 7.99, 'stock': 8},
+    'E2000017221101241890547C': {'nome' : "Pera"   , 'codigo' : 'E2000017221101241890547C', 'preco' : 2.50, 'stock': 18},
+    'E2000017221100961890544A': {'nome' : "Kiwi"   , 'codigo' : 'E2000017221100961890544A', 'preco' : 6.75, 'stock': 7},
 }
 
 caixas = {}
