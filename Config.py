552a95a5ec8211d49dfb695ffd7b4f10f32cbcc3
João Gabriel <gabriel.lima.almeida@gmail.com>
2023-09-11 20:48:10 -0300

    fix: dados,py removido

--- Config.py
+++ Config.py
@@ -1,11 +1,11 @@
-hostRFIDTeste = '192.168.68.109' # Local
+hostRFIDTeste = '221.1.1.102' # Local
 
 hostRFID = '172.16.103.0' # RFID
 portaRFID = 2598
 
-hostRFIDcliente = '192.168.68.109' 
+hostRFIDcliente = '221.1.1.102' 
 
-hostServidor = '192.168.68.109'
+hostServidor = '221.1.1.102'
 portaServidor = 1234
 
-ipDatabase = 'http://192.168.68.109:8000/'
+ipDatabase = 'http://221.1.1.102:8000/'
