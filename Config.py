b76f14e4442665cba8b7a4dc5ceecc9cc60f64c2
João Gabriel <gabriel.lima.almeida@gmail.com>
2023-09-12 13:08:25 -0300

    Fix: IPs das maquinas apresentação

--- Config.py
+++ Config.py
@@ -3,9 +3,9 @@ hostRFIDTeste = '221.1.1.102' # Local
 hostRFID = '172.16.103.0' # RFID
 portaRFID = 2598
 
-hostRFIDcliente = '221.1.1.102' 
+hostRFIDcliente = '172.16.103.0' 
 
-hostServidor = '221.1.1.102'
+hostServidor = '172.16.103.1'
 portaServidor = 1234
 
-ipDatabase = 'http://221.1.1.102:8000/'
+ipDatabase = 'http://172.16.103.2/'
